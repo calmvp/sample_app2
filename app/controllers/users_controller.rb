25ff4fd11285e967fc8d613aa40d354d6d95d120
Ned <nkaidayip@gmail.com>
2013-03-29 03:29:46 -0400

    Add user microposts

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,10 +1,15 @@
 class UsersController < ApplicationController
  before_filter :signed_in_user, only: [:index, :edit, :update, :destroy]
-before_filter :correct_user,   only: [:edit, :update]
+ before_filter :correct_user,   only: [:edit, :update]
  before_filter :admin_user,     only: :destroy
+  
+  def index
+    @users = User.paginate(page: params[:page])
+  end
   
   def show
   	@user = User.find(params[:id])
+    @microposts = @user.microposts.paginate(page: params[:page])
   end
 
   def new
@@ -21,45 +26,34 @@ before_filter :correct_user,   only: [:edit, :update]
   		render 'new'
   	end
   end
+
+  def edit
+  end
  
- def destroy
-  User.find(params[:id]).destroy
-    flash[:success] = "User destroyed."
-    redirect_to users_url
-  end
-
- def edit
-  end
-
-  def index
-    @users = User.paginate(page: params[:page])
-  end
-
-def update
+ def update
     if @user.update_attributes(params[:user])
-      flash[:success] = "Profile updated"
       sign_in @user
+      flash[:success] = "Profile updated"
       redirect_to @user
     else
       render 'edit'
     end
   end
 
+ def destroy
+  User.find(params[:id]).destroy
+    flash[:success] = "User destroyed."
+    redirect_to users_url
+  end
+
 private
-
-def admin_user
-  redirect_to(root_path) unless current_user.admin?
-end
-
-    def signed_in_user
-      unless signed_in?
-        store_location
-        redirect_to signin_url, notice: "Please sign in."
-      end
-    end
 
     def correct_user
       @user = User.find(params[:id])
       redirect_to(root_path) unless current_user?(@user)
     end
-end
+
+    def admin_user
+      redirect_to(root_path) unless current_user.admin?
+    end
+  end
